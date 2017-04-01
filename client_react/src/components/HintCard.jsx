8c69db774221d1a8797e4af0c0bf331bb2e050d6
Brian Jin <brianjin@Brians-MacBook-Air-2.local>
2017-03-31 20:21:22 -0700

    Refactor HintCard to class component toggle view hints

--- client_react/src/components/HintCard.jsx
+++ client_react/src/components/HintCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Card, Icon, Image, Button, Form, Label, TextArea } from 'semantic-ui-react';
+import HintEntry from './HintEntry.jsx';
 
 class HintCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      submittedHint: ''
+      submittedHint: '',
+      viewHints: false,
     }
     this.inputHint = this.inputHint.bind(this);
     this.onViewHints = this.onViewHints.bind(this);
@@ -19,72 +21,134 @@ class HintCard extends React.Component {
 
   onViewHints (e) {
     e.preventDefault();
-    console.log('ViewHints');
+    if (this.state.viewHints === false) {
+      this.setState({viewHints: true});
+    } 
+    else {
+      this.setState({viewHints: false});
+    }
   }
 
   onWriteHint (e) {
     e.preventDefault();
-    console.log('WriteHints');
   }
 
-  render () {    
-    return (
-      <div style={{width: 400, marginBottom: 200}}>
-        <Card style={{width: '100%', paddingBottom: 15}}>
-          <Card.Content>
+  render () { 
+    if(this.state.viewHints === false) {
+      return (
+        <div style={{width: 400, marginBottom: 200}}>
+          <Card style={{width: '100%', paddingBottom: 15}}>
+            <Card.Content>
+              <Card.Header>
+                Points: {this.props.points}
+              </Card.Header>
+            </Card.Content>
+            
+            <Image src={this.props.currentCard.pictureUrl} />
             <Card.Header>
-              Points: {this.props.points}
+              Liked Hints:
             </Card.Header>
-          </Card.Content>
-          
-          <Image src={this.props.currentCard.pictureUrl} />
-          <Card.Header>
-            SCORE: {props.score}
-          </Card.Header>
-        </Card.Content>
-
-        <Image src={props.currentCard.pictureUrl} />
-        <Card.Header>
-          HINTS:
-        </Card.Header>
-        <Card.Meta>
-          <span className='hint'>
-            Randomly selected liked hint
-          </span>
-        </Card.Meta>
-        <Card.Content extra>
-          <div className='ui two buttons'>
-            <Button
-              basic color='green'
-              onClick={props.onShowAnswer}
-            >
-            ANSWER
-            </Button>
-            <Button
-              basic color='yellow'
-              onClick={props.onHint}
-            >
-            HINT
-            </Button>
-          </div>
-        </Card.Content>
-      </Card>
-      <Form>
-        <Form.Field>
-          <TextArea
-            placeholder='Your Hint'
-            autoHeight/>
-        </Form.Field>
-          <Button>Submited Hints</Button>
-          <Button
-            size='medium'
-            // style={{float: 'right'}}
-            icon>
-            <Icon name='comment' />
-          </Button>
-      </Form>
-    </div>
-  )
+            <Card.Meta>
+              <span className='hint'>
+                Randomly selected liked hint
+              </span>
+            </Card.Meta>
+            <Card.Content extra>
+              <div className='ui two buttons'>
+                <Button
+                  basic 
+                  color='green'
+                  onClick={this.props.onShowAnswer}
+                >
+                Answer
+                </Button>
+                <Button
+                  basic color='yellow'
+                  onClick={this.props.onHint}
+                >
+                Hint
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+          <Form>
+            <Form.Field>
+              <TextArea 
+                onChange={this.inputHint}
+                placeholder='Your Hint' 
+                autoHeight/>
+            </Form.Field>
+              <Button
+                onClick={this.onViewHints}
+              >View Hints</Button>
+              <Button 
+                onClick={this.onWriteHint}
+                size='medium'
+                icon>
+                <Icon name='write' />
+              </Button>
+          </Form>
+        </div>
+      )
+    } else {
+      return (
+        <div style={{width: 400, marginBottom: 200}}>
+          <Card style={{width: '100%', paddingBottom: 15}}>
+            <Card.Content>
+              <Card.Header>
+                Points: {this.props.points}
+              </Card.Header>
+            </Card.Content>
+            
+            <Image src={this.props.currentCard.pictureUrl} />
+            <Card.Header>
+              Liked Hints:
+            </Card.Header>
+            <Card.Meta>
+              <span className='hint'>
+                Randomly selected liked hint
+              </span>
+            </Card.Meta>
+            <Card.Content extra>
+              <div className='ui two buttons'>
+                <Button
+                  basic 
+                  color='green'
+                  onClick={this.props.onShowAnswer}
+                >
+                Answer
+                </Button>
+                <Button
+                  basic color='yellow'
+                  onClick={this.props.onHint}
+                >
+                Hint
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+          <Form>
+            <Form.Field>
+              <TextArea 
+                onChange={this.inputHint}
+                placeholder='Your Hint' 
+                autoHeight/>
+            </Form.Field>
+              <Button
+                onClick={this.onViewHints}
+              >View Hints</Button>
+              <Button 
+                onClick={this.onWriteHint}
+                size='medium'
+                icon>
+                <Icon name='write' />
+              </Button>
+          </Form>
+          <HintEntry />
+        </div>
+      )
+    } 
+  }
 };
 
 export default HintCard;
