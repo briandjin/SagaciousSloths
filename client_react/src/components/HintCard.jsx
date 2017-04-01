fa27a40b92df808e4bd91cae3ae64d47b6c9af36
Brian Jin <brianjin@Brians-MacBook-Air-2.local>
2017-03-31 19:06:54 -0700

    Add functionality to HintCard input and buttons to submit hints

--- client_react/src/components/HintCard.jsx
+++ client_react/src/components/HintCard.jsx
@@ -1,58 +1,89 @@
 import React from 'react';
 import { Card, Icon, Image, Button, Form, Label, TextArea } from 'semantic-ui-react';
 
-var HintCard = function (props) {
-  return (
-    <div style={{width: 400, marginBottom: 200}}>
-      <Card style={{width: '100%', paddingBottom: 15}}>
-        <Card.Content>
+class HintCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submittedHint: ''
+    }
+    this.inputHint = this.inputHint.bind(this);
+    this.onViewHints = this.onViewHints.bind(this);
+    this.onWriteHint = this.onWriteHint.bind(this);
+  }
+
+  inputHint (e) {
+    this.setState({submittedHint: e.target.value});
+    console.log(e.target.value);
+  }
+
+  onViewHints (e) {
+    e.preventDefault();
+    console.log('ViewHints');
+  }
+
+  onWriteHint (e) {
+    e.preventDefault();
+    console.log('WriteHints');
+  }
+
+  render () {    
+    return (
+      <div style={{width: 400, marginBottom: 200}}>
+        <Card style={{width: '100%', paddingBottom: 15}}>
+          <Card.Content>
+            <Card.Header>
+              Points: {this.props.points}
+            </Card.Header>
+          </Card.Content>
+          
+          <Image src={this.props.currentCard.pictureUrl} />
           <Card.Header>
-            Points: {props.points}
+            Liked Hints:
           </Card.Header>
-        </Card.Content>
-        
-        <Image src={props.currentCard.pictureUrl} />
-        <Card.Header>
-          Liked Hints:
-        </Card.Header>
-        <Card.Meta>
-          <span className='hint'>
-            Randomly selected liked hint
-          </span>
-        </Card.Meta>
-        <Card.Content extra>
-          <div className='ui two buttons'>
+          <Card.Meta>
+            <span className='hint'>
+              Randomly selected liked hint
+            </span>
+          </Card.Meta>
+          <Card.Content extra>
+            <div className='ui two buttons'>
+              <Button
+                basic 
+                color='green'
+                onClick={this.props.onShowAnswer}
+              >
+              Answer
+              </Button>
+              <Button
+                basic color='yellow'
+                onClick={this.props.onHint}
+              >
+              Hint
+              </Button>
+            </div>
+          </Card.Content>
+        </Card>
+        <Form>
+          <Form.Field>
+            <TextArea 
+              onChange={this.inputHint}
+              placeholder='Your Hint' 
+              autoHeight/>
+          </Form.Field>
             <Button
-              basic color='green'
-              onClick={props.onShowAnswer}
-            >
-            Answer
+              onClick={this.onViewHints}
+            >View Hints</Button>
+            <Button 
+              onClick={this.onWriteHint}
+              size='medium'
+              icon>
+              <Icon name='write' />
             </Button>
-            <Button
-              basic color='yellow'
-              onClick={props.onHint}
-            >
-            Hint
-            </Button>
-          </div>
-        </Card.Content>
-      </Card>
-      <Form>
-        <Form.Field>
-          <TextArea 
-            placeholder='Your Hint' 
-            autoHeight/>
-        </Form.Field>
-          <Button>Submited Hints</Button>
-          <Button 
-            size='medium'
-            // style={{float: 'right'}}
-            icon>
-            <Icon name='comment' />
-          </Button>
-      </Form>
-    </div>
-  )
+        </Form>
+      </div>
+    )
+  }
 };
 
 export default HintCard;
