afde901b282723e1d660994746f0c55425659140
Allen An Le <allenanle16@gmail.com>
2017-03-31 19:03:19 -0700

    Shuffle the selected cards

--- server/request-handlers.js
+++ server/request-handlers.js
@@ -182,7 +182,19 @@ var getLegacyCards = function (req, res) {
         }
       })
     });
-    console.log('---> selectedCards', selectedCards);
+
+    var shuffle = function(array) {
+      var j, x, i;
+      for (i = array.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = array[i - 1];
+        array[i - 1] = array[j];
+        array[j] = x;
+      }
+    };
+
+    shuffle(selectedCards);
+
     res.status(200).send(selectedCards);
   })
 };
