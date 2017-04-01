964cde4633cdc2e441db60e3e346be3df367beb9
Brian Jin <brianjin@Brians-MacBook-Air-2.local>
2017-03-31 19:14:47 -0700

    Add functionality to sumbit and view hints

--- client_react/src/components/HintCard.jsx
+++ client_react/src/components/HintCard.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import { Card, Icon, Image, Button, Form, Label, TextArea } from 'semantic-ui-react';
 
-var HintCard = function (props) {
-  return (
-    <div style={{width: 400, marginBottom: 200}}>
-      <Card style={{width: '100%', paddingBottom: 15}}>
-        <Card.Content>
+class HintCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submittedHint: ''
+    }
+    this.inputHint = this.inputHint.bind(this);
+    this.onViewHints = this.onViewHints.bind(this);
+    this.onWriteHint = this.onWriteHint.bind(this);
+  }
+
+  inputHint (e) {
+    this.setState({submittedHint: e.target.value});
+    console.log(e.target.value);
+  }
+
+  onViewHints (e) {
+    e.preventDefault();
+    console.log('ViewHints');
+  }
+
+  onWriteHint (e) {
+    e.preventDefault();
+    console.log('WriteHints');
+  }
+
+  render () {    
+    return (
+      <div style={{width: 400, marginBottom: 200}}>
+        <Card style={{width: '100%', paddingBottom: 15}}>
+          <Card.Content>
+            <Card.Header>
+              Points: {this.props.points}
+            </Card.Header>
+          </Card.Content>
+          
+          <Image src={this.props.currentCard.pictureUrl} />
           <Card.Header>
             SCORE: {props.score}
           </Card.Header>
